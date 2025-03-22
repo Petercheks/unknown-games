b873215d566aa386b8b2677b103e1864afeec7cc
Pedro Matos <pedrojmatos94@gmail.com>
2025-03-21 22:03:53 -0300

    movimiento horizontal

---
+++ unknow-game/Assets/Scripts/Movimiento/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovementRb : MonoBehaviour
+{
+    private float horizontal;
+    private float speed = 8f;
+    [SerializeField] private Rigidbody2D rb;
+
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+}
